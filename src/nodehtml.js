c045f7bfce7109e17c74d9a3eee4c51fd075af70
pablojg9 <pablojuniorgn2@gmail.com>
2020-04-16 20:35:15 -0300

    Primeira vez usando ExpressJS

---
+++ src/nodehtml.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/html/index.html")
+});
+
+app.get('/sobre', (req, res) => { 
+    res.sendFile(__dirname + "/html/sobre.html")
+    
+});
+
+app.listen(3000);
